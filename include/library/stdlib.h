5926bdb2f7211e4c1f7b976eff70779e6fbc3171
owenwijaya22 <owenwijaya89@gmail.com>
2024-03-26 16:24:16 +0800

    got rid of hard code atoi

--- include/library/stdlib.h
+++ include/library/stdlib.h
@@ -1,6 +1,6 @@
 #ifndef LIBRARY_STDLIB_H
 #define LIBRARY_STDLIB_H
-
+int atoi(const char *str);
 void exit( int code );
 
 #endif
--- user/process1.c
+++ user/process1.c
@@ -1,20 +1,7 @@
 #include "library/syscalls.h"
 #include "library/string.h"
 #include "library/stdio.h"
-int atoi(const char *str)
-{
-    int res = 0;
-    int sign = 1;
-    int i = 0;
-    if (str[0] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    for (; str[i] != '\0'; ++i)
-        res = res * 10 + str[i] - '0';
-    return sign * res;
-}
+#include "library/stdlib.h"
 
 void runForSeconds(int seconds) {
     unsigned int startTime;
